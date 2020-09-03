2f4261aedc35514319e740f4a8e0cf22d0f34f1b
Chris Roche <croche@paulsoninstitute.org>
2020-09-03 13:34:07 -0500

    changed interval

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ fs.readdir(path.join( __dirname, '/images/'), (err, files) => {
   unusedImages = [...imagesArr];
 
   console.log('cron is running...');
-  cron.schedule('5 13 * * 1-5', () => {
+  cron.schedule('35 13 * * 1-5', () => {
     console.log(' ');
     tweet();
   });
