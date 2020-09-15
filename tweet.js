b6026c6a7c8375844a10bf11b2edf0c09d43a73c
Chris Roche <croche@paulsoninstitute.org>
2020-09-15 16:01:28 -0500

    added read/write from file

--- tweet.js
+++ tweet.js
@@ -3,7 +3,7 @@ const fs = require( 'fs' ),
   path = require( 'path' ),
   Twit = require( 'twit' ),
   cron = require( 'node-cron' ),
-  https = require('https');
+  https = require( 'https' );
 const config = {
   consumer_key:         'TfwVdFJbuzqvBdSAqeVwnYLuZ',
   consumer_secret:      '5GZNiTRQM7eBvlFRxShsWwD7bOLdKYv5zcx3OFhduS31YXUavj',
@@ -16,48 +16,55 @@ let imagePath;
 let b64content;
 let url;
 let title;
-
-const imagesArr = [
-  'AI_Commentary_2$americas-got-ai-talent-us-big-lead-in-ai-research-is-built-on-importing-researchers.png',
-  'AI_talent_commentary_1$americas-got-ai-talent-us-big-lead-in-ai-research-is-built-on-importing-researchers.png',
-  'AI_Talent_Screenshot_3$americas-got-ai-talent-us-big-lead-in-ai-research-is-built-on-importing-researchers.png',
-  'Billion_Users_$the-chinese-and-american-tech-apps-winning-the-next-billion-users.png',
-  'Chips_on_the_shoulder_2$china-chips-semiconductors-artificial-intelligence.png',
-  'Chips_on_the_shoulder1$china-chips-semiconductors-artificial-intelligence.png',
-  'Chips_on_the_shoulder3$china-chips-semiconductors-artificial-intelligence.png',
-  'ev_new_energy_1$analysis-china-electric-vehicle-ev-industry.png',
-  'EV_new_energy_2$analysis-china-electric-vehicle-ev-industry.png',
-  'ev_new_energy_3$analysis-china-electric-vehicle-ev-industry.png',
-  'Great_Power_1$china-great-power-foreign-policy-covid19.png',
-  'Great_Power_2$china-great-power-foreign-policy-covid19.png',
-  'Great_Power_3$china-great-power-foreign-policy-covid19.png',
-  'Great_Power_4$china-great-power-foreign-policy-covid19.png',
-  'Interest_rate$china-interest-rate-cut-economy.png',
-  'Much_ado_about_data1$ai-data-us-china.png',
-  'Retirement_1$why-beijing-has-resisted-raising-the-retirement-age.png',
-  'Retirement_2$why-beijing-has-resisted-raising-the-retirement-age.png',
-  'SHFTZ_1$shanghai-free-trade-zone-reform-opening.png',
-  'SHFTZ_2$shanghai-free-trade-zone-reform-opening.png',
-  'SHFTZ_4$shanghai-free-trade-zone-reform-opening.png',
-  'SHFTZ3$shanghai-free-trade-zone-reform-opening.png',
-];
+let imagesArr;
 
 run();  
 
 // FUNCTIONS
 function run() {
   console.log('fetching random image...');
-  let img = pickRandomNum(imagesArr);
-  console.log(img)
-  tweet( img );
+  fs.readFile('UNUSED_IMAGES.txt', 'utf-8', (err, data) => {
+    if(err) throw err;
+
+    // pick image from unused images list
+    imagesArr = data.split(',\n');
+    console.log(imagesArr)
+    let num = pickRandomNum(imagesArr);
+    console.log(num)
+    let unusedTxt;
+
+    // tweet
+    // tweet( num );
+
+    // remove image from unused images list
+    if(imagesArr.length < 2) {
+      fs.readFile('ALL_IMAGES.txt', 'utf-8', (errr, response) => {
+        console.log(response)
+        unusedTxt = response;
+        fs.writeFile('UNUSED_IMAGES.txt', unusedTxt, (error) => {
+          if(error) throw error;
+          console.log('**** Successful reset images list. ****');
+        })
+      })
+    } else {
+      imagesArr.splice(num, 1)
+      unusedTxt = imagesArr.join(',\n')
+      fs.writeFile('UNUSED_IMAGES.txt', unusedTxt, (error) => {
+        if(error) throw error;
+        console.log('**** Successful edit file. New length: ', imagesArr.length, '****');
+      })
+    }
+
+    console.log('unusedTxt:', unusedTxt)
+  })
 }
 
 function pickRandomNum(arr) {
   return Math.floor( Math.random() * arr.length )
 }
 
-function tweet( img ){
-  imagePath = 'https://macropolo-s3.s3.us-east-2.amazonaws.com/twitter_charts/' + imagesArr[img];
+function tweet( num ){
+  imagePath = 'https://macropolo-s3.s3.us-east-2.amazonaws.com/twitter_charts/' + imagesArr[num];
   console.log(imagePath);
 
   https.get(imagePath, (resp) => {
